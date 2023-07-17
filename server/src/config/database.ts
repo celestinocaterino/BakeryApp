6bbe7b15aca7595867bfad19f5839c7e17d23b2d
Celestino Caterino <celestecaterino@gmail.com>
2023-07-17 19:16:55 +0200

    updated server config variables

---
+++ server/src/config/database.ts
@@ -0,0 +1,15 @@
+import { DataSource } from 'typeorm';
+import config from './config';
+
+export const dataSource = new DataSource({
+    type: 'mysql',
+    host: config.node_env == 'development' ? 'localhost' : 'mysql_db',
+    port: config.node_env == 'development' ? 9906 : 3306,
+    username: 'MYSQL_USER',
+    password: 'MYSQL_PASSWORD',
+    database: 'bakery_app',
+    entities: ['dist/entities/**/*{.js,.ts}'],
+    migrations: ['dist/migrations/**/*{.js,.ts}'],
+    logging: true,
+    synchronize: false,
+});
